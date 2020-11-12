edf754c8a4a314c8aa925a360daa561a6350025d
unknown <siriapps3@gmail.com>
2020-11-12 12:14:14 +0530

    string challenge

--- doormat.py
+++ doormat.py
@@ -1,1 +1,20 @@
-#this is a Hackerrank doormat problem
+# https://www.hackerrank.com/challenges/designer-door-mat/problem
+
+size = list(map(int,input().split()))
+rows = size[0]
+columns = size[1]
+
+# printing the top half of the mat
+for i in range(rows // 2):
+    printString = ".|." * ((i * 2) + 1)
+    print(printString.center(columns, "-"))
+
+print('WELCOME'.center(columns, '-'))
+
+# printing the bottom half of the mat
+for i in reversed(range(rows // 2)):
+    printString = ".|." * ((i * 2) + 1)
+    print(printString.center(columns, "-"))
+
+
+
