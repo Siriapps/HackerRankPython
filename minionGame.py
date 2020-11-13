aaac6109f613a1085f277c50bbb4fe0902b9e941
unknown <siriapps3@gmail.com>
2020-11-13 09:44:40 +0530

    String Challenge

--- minionGame.py
+++ minionGame.py
@@ -1,3 +1,4 @@
+# https://www.hackerrank.com/challenges/the-minion-game/problem
 def minion_game(string):
     string = string.upper()
     vowels = ['A', 'E', 'I', 'O', 'U']
@@ -6,11 +7,17 @@ def minion_game(string):
     stusc = 0
 
     for i in range(len(string)):
+        # checking whether the letter is vowel
         if string[i] in vowels:
-            kevsc += (len(string) - i)
-        else:
-            stusc += (len(string) - i)
+            kevsc += (len(string) - i)  # setting the sum of all the lengths of the possible strings
+                                        # i.e the length from the vowel till the end as the score
 
+        # checking whether the letter is consonent
+        else:
+            stusc += (len(string) - i)  # setting the sum of all the lengths of the possible strings
+                                        # i.e the length from the consonent till the end as the score
+
+    # Checking whose score is greater so as to print their 'name and score' or declare the match as 'draw'
     if kevsc > stusc:
         print("Kevin", kevsc)
     elif stusc > kevsc:
