d78f51f116a7119fc92264e81fcd08e892173fd3
unknown <siriapps3@gmail.com>
2020-11-30 22:00:40 +0530

    Basic Data types

--- nested-lists.py
+++ nested-lists.py
@@ -1,23 +1,11 @@
-l = []
-second_lowest_names = []
-scores = set()
+# https://www.hackerrank.com/challenges/nested-list/problem
+myList = []
+for n in range(int(input())):
+    myList.append([input(), float(input())]) #Ex- mylist=[['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
 
-for _ in range(int(input("Enter How many students: "))):
-    name = input("Enter name: ")
-    score = float(input("Enter score: "))
-    l.append([name, score])
-    scores.add(score)# adding the scores to the scores in which set function helps us only take
-                     # unique values and it also sorts the values helping us to find the second_lowest score
-
-        
-second_lowest = sorted(scores)[1] #this sorts the scores in descending order
-                                  # and grabs the first number which is the second lowest score
+secondLowest = sorted(list(set(score for (name, score) in myList)))[1]# this sorts the unique scores in descending order
+                                                                      # and grabs the first number which is the second lowest score
 
 # to check the condition if two or more people got the second lowest
 # if so then it prints the sorted names
-for name, score in l:
-    if score == second_lowest:
-        second_lowest_names.append(name)
-
-for name in sorted(second_lowest_names):
-    print(name, end='\n')
+print("\n".join(sorted(list(name for (name, score) in myList if score == secondLowest))))
