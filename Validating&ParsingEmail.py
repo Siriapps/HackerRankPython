c38648db58ff6aa34ee564f154990151fc96ac72
unknown <siriapps3@gmail.com>
2020-12-15 07:41:46 +0530

    regex and parsing challenge

--- Validating&ParsingEmail.py
+++ Validating&ParsingEmail.py
@@ -4,6 +4,6 @@ import email.utils
 for _ in range(int(input())):
     mail = email.utils.parseaddr(input())
     if re.match(r'^([a-zA-Z0-9][-._]*)+@([a-zA-Z]+)\.([a-zA-Z]){1,3}$',mail[1]):
-        print("out:",email.utils.formataddr(mail))
+        print(email.utils.formataddr(mail))
     else:
         continue
