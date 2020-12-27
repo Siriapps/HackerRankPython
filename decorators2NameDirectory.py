131afa5f49db7afa87f2f17e4a3bf0d4a15a6794
unknown <siriapps3@gmail.com>
2020-12-27 22:22:05 +0530

    Closure and decorator challenge

--- decorators2NameDirectory.py
+++ decorators2NameDirectory.py
@@ -1,12 +1,12 @@
 # https://www.hackerrank.com/challenges/decorators-2-name-directory/problem
 
 # here our task is to return each list from the sorted nested list to the function 'name_format' by using decorator
-def person_lister(f):
+def person_lister(func):
     def inner(people):
-        return [f(i) for i in sorted(people,key=lambda x:(x[2]))]  # sort by age i.e in 2nd index of each person's list
+        return [func(person) for person in sorted(people,key=lambda x:int(x[2]))]  # sort by age i.e in 2nd index of each person's list
     return inner
 
-@person_lister
+@person_lister # --> person_lister(name_format)
 def name_format(person):
     return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
 
